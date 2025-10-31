74c2ebd99a2cf2bea734d0110ae448958c99dcb9
Wayne <decaturw@upstate.edu>
2025-10-31 16:06:56 -0400

    add test of the suqingdong pmc-id-converter

--- tests/test_Converter.py
+++ tests/test_Converter.py
@@ -13,10 +13,10 @@ import filecmp
 from bs4 import BeautifulSoup
 
 # Run this file while working directory is at root,
-# like `pytest -v tests/test_PubMed_Central_ID_Converter_for_humans.py` 
+# like `pytest -v tests/test_Converter.py` 
 # -or-
 # if want printing from the pytests tests
-# like `pytest -s -v tests/test_PubMed_Central_ID_Converter_for_humans.py` 
+# like `pytest -s -v tests/test_Converter.py` 
 
 # Compares the results of my `PubMed_Central_ID_Converter_for_humans.py` to the 
 # results from pmc-id-converter (https://pypi.org/project/pmc-id-converter/) as
@@ -108,7 +108,7 @@ def test_pmc_id_converter_working_as_expected():
     the_old_query_result_list = ['23193287', None, '23193288']
     from pmc_id_converter import API
     example_result_a = API.idconv('PMC3531190')[0].data
-    example_results_b = API.idconv('23193287')[0].data
+    example_result_b = API.idconv('23193287')[0].data
     assert example_result_a == the_old_result_dict, "Result of `API.idconv('PMC3531190')[0].data` is not matching expected."
     assert example_result_b == the_old_result_dict, "Result of `API.idconv('23193287')[0].data` is not matching expected."
     the_pmids = []
