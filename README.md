d4d53078f287ca629d702d91138825493b45ed26
Wayne <decaturw@upstate.edu>
2025-11-03 11:29:38 -0500

    note about API limits and how nelson aloysio's pubmed-id package overcomes this limitation

--- README.md
+++ README.md
@@ -22,6 +22,14 @@ Use the library to get PubMed identifiers (PMIDs), PubMed Central identifiers, i
 In an opionated choice, the default format for the results is a Pandas dataframe; however, this can be adjusted via flag/argument settings.  
 It is largely Python-related kernel agnostic, which means you can use it in JupyterLite as well as in more standard Jupyter where a typical ipykernel is involved. At present, command line use of the library is not compatible with Pyodide/JupyterLite. Command line use is fully allowed in a typical system with a POSIX system shell (Bash/zsh-type) where typical Python is installed.    
 
+**Limitations**
+
+Keep in mind according to [the PMC ID Converter API](https://pmc.ncbi.nlm.nih.gov/tools/id-converter-api/), the basis for this package's coverting identifiers functionality:  
+>"The PMC ID Converter API will only return related IDs if the article is in PubMed Central (PMC)."
+
+This means it won't return identifiers for those articles not present in PubMEd Central.
+
+(Note that [nelsonaloysio's package `pubmed-id`](https://pypi.org/project/pubmed-id/) discussed under the 'Influences' section below, goes beyond using the API and can do some webscaping, supposedly, and so it is able to return identifiers for those not present in PubMed Central, see [here](https://github.com/nelsonaloysio/pubmed-id#scrape-data-from-website) about , "Note: some papers are unavailable from the API, but still return data when scraped, e.g., PMID 15356126". Therefore, [nelsonaloysio's package `pubmed-id`](https://pypi.org/project/pubmed-id/) is able to return identifiers for those not present in PubMed Central)
 
 ---------
 
--- tests/test_Converter.py
+++ tests/test_Converter.py
@@ -75,7 +75,19 @@ def write_string_to_file(s, fn):
 
 
 
+def compare_files_content(file1_path, file2_path):
+    '''
+    Compare content ignoring whitespace differences
+    '''
+    data1 = read_and_process_file4content_check(file1_path)
+    data2 = read_and_process_file4content_check(file2_path)
 
+    assert len(data1) == len(data2), f"Files have different number of lines: {len(data1)} vs {len(data2)}"
+
+    for i, (line1, line2) in enumerate(zip(data1, data2), 1):
+        assert line1 == line2, f"Difference found at line {i}:\nFile 1: {' '.join(line1)}\nFile 2: {' '.join(line2)}"
+
+    return True  # If we get here, the files are identical in content
 def compare_file_content_equality(file1_path, file2_path, msg="Files are not identical in content, ignoring whitespace differences."):
     '''
     Compare two files content ignoring whitespace differences
@@ -104,29 +116,87 @@ def compare_file_content_equality(file1_path, file2_path, msg="Files are not ide
 # Check the pmc-id-converter is getting same results as it did in the past
 #--------------------------------------------------------------------------#
 # Since I am relying on ththe pmc-id-converter for comparison, I'm checking 
-# the current version is working as expected.
-def test_pmc_id_converter_working_as_expected():
+# the current version is working as expected before comparing any results from 
+# my script.
+def test_pmc_id_converter_cli_working_as_expected():
+    the_old_result_text = '{"doi": "10.1007/s13205-018-1330-z", "pmcid": "PMC6039336", "pmid": 30003000, "requested-id": "30003000"}\n{"doi": "10.1002/open.201800095", "pmcid": "PMC6031859", "pmid": 30003001, "requested-id": "30003001"}\n{"doi": "10.1002/open.201800044", "pmcid": "PMC6031856", "pmid": 30003002, "requested-id": "30003002"}\n'
+    os.system('pmc_idconv 30003000 30003001 30003002 > current_result.txt')
+    with open('current_result.txt', 'r') as myfile:
+        current_result_text=myfile.read()
+    '''
+    with open('current_result.json', 'r') as f:
+        result_list = json.load(f)
+    current_result_list_string = ("\n").join(result_list)
+    '''
+    assert current_result_text == the_old_result_text, "Result of `pmc_idconv 30003000 30003001 30003002 > current_result.json` is not matching expected." 
+
+def test_pmc_id_converter_as_function_working_as_expected():
     original_stderr = sys.stderr
     sys.stderr = StringIO()  # Redirect stderr to a dummy buffer ;  this is 
     # because `PMC3531191123` is not a match and will produce `[2025-10-31 20:11:21 ID_CONV_API idconv ERROR MainThread:58] RecordError: Identifier not found in PMC for "PMC3531191123"`. The test will still give 'PASSED', but things will look bad. By shunting std.err to a dummy buffer, it avoids this passing through and making things look bad.
     the_old_result_dict = {'doi': '10.1093/nar/gks1195', 'pmcid': 'PMC3531190', 'pmid': 23193287, 'requested-id': 'PMC3531190'}
     the_old_query_result_list = ['23193287', None, '23193288']
     from pmc_id_converter import API
-    example_result_a = API.idconv('PMC3531190')[0].data
-    example_result_b = API.idconv('23193287')[0].data
-    assert example_result_a == the_old_result_dict, "Result of `API.idconv('PMC3531190')[0].data` is not matching expected."
-    assert example_result_b['doi'] == the_old_result_dict['doi'], "Result of `API.idconv('23193287')[0].data` is not matching expected."
+    current_example_result_a = API.idconv('PMC3531190')[0].data
+    current_example_result_b = API.idconv('23193287')[0].data
+    assert current_example_result_a == the_old_result_dict, "Result of `API.idconv('PMC3531190')[0].data` is not matching expected."
+    assert current_example_result_b['doi'] == the_old_result_dict['doi'], "Result of `API.idconv('23193287')[0].data` is not matching expected."
     the_pmids = []
     query_ids = 'PMC3531190, PMC3531191123, PMC3531191'
     try: # see the `sys.stderr = StringIO()` line above
         records_of_query_results = API.idconv(query_ids)
         for record in records_of_query_results:
             the_pmids.append(record.data.get('pmid'))
-        the_pmids = [str(x) if isinstance(x, int) else x for x in the_pmids] # otherwise they'll be integers which isn't what we really want as these are idenitifiers and not numbers to process in math
-        assert the_pmids == the_old_query_result_list, "Result of `API.idconv(query_ids)` is not matching expected."
+        current_the_pmids = [str(x) if isinstance(x, int) else x for x in the_pmids] # otherwise they'll be integers which isn't what we really want as these are idenitifiers and not numbers to process in math
+        assert current_the_pmids == the_old_query_result_list, "Result of `API.idconv(query_ids)` is not matching expected."
     finally:
             sys.stderr = original_stderr # Restore original stderr ; see 
             # the `sys.stderr = StringIO()` line above
+
+
+# Check the `PubMed_Central_ID_Converter_for_humans` script is working as 
+# expected 
+#--------------------------------------------------------------------------#
+
+# Have one test at least test fetching the data works in pyodide or Python?
+def test_converter_for_humans_gets_data_from_API_whether_Pyodide_or_Python():
+    # MAYBE IF CAN. BUT HOW TO RUN PYTEST SINCE NO COMMAND LINE!?!
+    # Answer see https://pypi.org/project/pytest-pyodide/ under 
+    #'run_in_pyodide' 
+    #...AND /OR...
+    # maybe make a separate test file and use the following:
+    '''
+    import pytest
+    # Run tests within a specific package
+    pytest.main(['--pyargs', 'your_package.tests'])
+    # You can also pass other pytest arguments
+    # pytest.main(['--pyargs', 'your_package.tests', '-k', 'specific_test_name'])
+    '''
+    pass
+
+# First check cli ability.
+def test_converter_for_humans_cli_working_as_expected():
+    # Check makes pandas and result as expected by converting pmc_id_converter result to Pandas as I worked out in https://github.com/fomightez/pmc_id_converter_demo-binder
+
+    # Check you can make a dictionary and result same as pmc_id_converter
+
+    # Check you can make json and result same as if pmc_id_converter result converted to json
+    pass
+
+# Now check when used as a function
+def test_converter_for_humans_cli_working_as_expected():
+    # Check makes pandas and result as expected by converting pmc_id_converter result to Pandas as I worked out in https://github.com/fomightez/pmc_id_converter_demo-binder
+
+    # Check you can make a dictionary and result same as pmc_id_converter
+
+    # Check you can make json and result same as if pmc_id_converter result converted to json
+    pass
+
+
+
+
+
+
 
 
 '''
