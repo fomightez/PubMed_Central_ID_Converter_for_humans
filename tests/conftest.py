9ff849735a8e6d05ff9a2b84652eb69af76dd1e1
fomightez <decaturw@upstate.edu>
2025-11-22 15:19:43 -0500

    add way to use freshest in-session code for pytest tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,13 @@ import importlib
 # This runs the equivalent of `uv pip install -e .` at the start of the session so that uv registers the entrypoint `PMC_id_convert` for the script as a command that can be called on the command line
 '''
 During development (now):
-I need `uv pip install -e .` in tests because the CLI entry point isn't registered yet.
-After publishing to PyPI:
-Users will have already done pip install PMC-ID-Converter-for-humans, so the PMC_id_convert command is already available. No installation needed in tests.
+I needed `uv pip install -e .` at the start of test pahse because the CLI entry point wassn't registered yet.
+After using `pip install git+https://github.com/fomightez/PubMed_Central_ID_Converter_for_humans.git` to install the current github version or publishing to PyPI(or TestPyPI) and then users using `pip install`:
+MyBinder session that comes (or users local use) will have already done `pip install` PMC-ID-Converter-for-humans (or equivalent), so the PMC_id_convert command is already available. No installation needed in tests.
+However, I left a way in the notebook `test_the_package.ipynb` to add running `uv pip install -e .` again to use the current in-session code for the package by setting
+`use_freshest_in_session_package_code` to `True`. This leaves a way to actively test
+what is changed in the session instead of just testing what was `pip` installed, 
+which is the default for now.
 '''
 #SO I ADDED testing and skipping the `uv pip install -e .` if `pip install` of the packge already somehow or other occured. 
 @pytest.fixture(scope="session", autouse=True)
